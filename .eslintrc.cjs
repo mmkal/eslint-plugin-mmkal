8be194954d5e90a1122d5b7dbe437f4cbd5ba7db
Misha Kaletsky <mmkal@users.noreply.github.com>
2023-11-21 20:49:13 -0500

    bump codegen

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,3 +4,7 @@ module.exports = require('./src/index').getRecommended()
 // they come from the npm package of this package so we don't have the updates yet 🧐
 module.exports.rules['mmkal/unicorn/expiring-todo-comments'] = 'off'
 module.exports.rules['mmkal/unicorn/no-empty-file'] = 'off'
+
+module.exports.plugins.push('codegen')
+module.exports.rules['mmkal/codegen/codegen'] = 'off'
+module.exports.rules['codegen/codegen'] = 'warn'
