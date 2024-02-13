45bc093b0d078397cb06c9f84b0689aeaa7a2d9d
Misha Kaletsky <mmkal@users.noreply.github.com>
2024-02-12 21:38:59 -0500

    update test

--- test/index.test.js
+++ test/index.test.js
@@ -14,14 +14,14 @@ test('fix works', async () => {
   fs.mkdirSync(testdir, {recursive: true})
   fs.readdirSync(testdir).forEach(file => fs.unlinkSync(path.join(testdir, file)))
   fs.writeFileSync(typescript, 'export const a = 1;')
-  fs.writeFileSync(esm, `import * as fs from 'fs';\nexport const read = fs.readFile;`)
-  fs.writeFileSync(commonjs, `const fs = require('fs');\nexport const read = fs.readFile;`)
+  fs.writeFileSync(esm, `import * as fs from 'node:fs';\nexport const read = fs.readFile;`)
+  fs.writeFileSync(commonjs, `const fs = require('node:fs');\nexport const read = fs.readFile;`)
 
   await execa('pnpm', ['eslint', 'test/ignoreme/*', '--fix', '--no-ignore'])
   /** @type {(file: string) => string} */
   const read = file => fs.readFileSync(file).toString().trim()
 
   expect(read(typescript)).toEqual('export const a = 1')
-  expect(read(esm)).toEqual(`import * as fs from 'fs'\n\nexport const read = fs.readFile`)
-  expect(read(commonjs)).toEqual(`const fs = require('fs')\n\nexport const read = fs.readFile`)
+  expect(read(esm)).toEqual(`import * as fs from 'node:fs'\nexport const read = fs.readFile`)
+  expect(read(commonjs)).toEqual(`const fs = require('node:fs')\nexport const read = fs.readFile`)
 }, 10_000)
