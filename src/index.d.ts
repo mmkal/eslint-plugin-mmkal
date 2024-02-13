853fcc3defae4e0c2802fd7b50405af836cdd93c
Misha Kaletsky <mmkal@users.noreply.github.com>
2024-02-12 19:31:36 -0500

    avoid export = style

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,9 @@
 import {type Plugin} from 'eslint-plugin-wrapper'
 
-declare const plugin: Plugin & {
-  getRecommended: () => NonNullable<Plugin['configs']>[string]
-}
+declare const plugin: Plugin
 
-export = plugin
+export declare const getRecommended: () => NonNullable<Plugin['configs']>[string]
+export declare const configs: Plugin['configs']
+export declare const rules: Plugin['rules']
+export declare const environments: Plugin['environments']
+export declare const processors: Plugin['processors']
