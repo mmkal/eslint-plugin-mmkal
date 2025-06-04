39912cafc1152447584b0d67a05485b58a38236c
Misha Kaletsky <mmkal@users.noreply.github.com>
2025-06-04 11:47:33 -0400

    bump timeout?

--- test/mmkal-repos.test.ts
+++ test/mmkal-repos.test.ts
@@ -22,7 +22,7 @@ for (const repoUrl of reposToTest) {
       const clone = path.join(tmp, repoName)
       expect(fs.existsSync(clone)).toBe(true)
       await execa('pnpm', ['install'], {cwd: clone})
-      await execa(path.join(process.cwd(), 'node_modules', '.bin', 'link'), [process.cwd()], {cwd: clone})
+      await execa(path.join(process.cwd(), 'node_modules/.bin/link'), [process.cwd()], {cwd: clone})
       const {all: lint} = await execa('pnpm', ['eslint', '.', '--fix', '--max-warnings', '0'], {
         all: true,
         cwd: clone,
@@ -37,6 +37,6 @@ for (const repoUrl of reposToTest) {
         .replaceAll(/\d+:\d+/g, '<line>:<col>')
       expect(snapshot).toMatchSnapshot()
     },
-    {timeout: 30_000},
+    {timeout: 60_000},
   )
 }
