b3f0bde30b70a701fcb2a767347c8572907095b8
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2025-09-22 22:38:13 +0100

    lint

--- test/mmkal-repos.test.ts
+++ test/mmkal-repos.test.ts
@@ -10,37 +10,37 @@ const reposToTest = [
 const ts = Date.now().toString()
 
 for (const repoUrl of reposToTest) {
-  test(
-    `lint ${repoUrl}`,
-    {timeout: 60_000},
-    async () => {
-      const {execa} = await import('execa')
-      await execa('pnpm', ['build'])
-      const tmp = path.join(`/tmp/eslint-plugin-mmkal-testing`, ts)
-      fs.mkdirSync(tmp, {recursive: true})
-      await execa('git', ['clone', repoUrl], {cwd: tmp})
-      const repoName = repoUrl.split('/').pop()!
-      const clone = path.join(tmp, repoName)
-      expect(fs.existsSync(clone)).toBe(true)
-      await execa('pnpm', ['install'], {cwd: clone})
-      await execa(path.join(process.cwd(), 'node_modules/.bin/link'), [process.cwd()], {cwd: clone})
-      const {all: lint} = await execa(path.join(process.cwd(), 'node_modules/.bin/eslint'), ['.', '--fix', '--max-warnings', '0'], {
+  test(`lint ${repoUrl}`, {timeout: 60_000}, async () => {
+    const {execa} = await import('execa')
+    await execa('pnpm', ['build'])
+    const tmp = path.join(`/tmp/eslint-plugin-mmkal-testing`, ts)
+    fs.mkdirSync(tmp, {recursive: true})
+    await execa('git', ['clone', repoUrl], {cwd: tmp})
+    const repoName = repoUrl.split('/').pop()!
+    const clone = path.join(tmp, repoName)
+    expect(fs.existsSync(clone)).toBe(true)
+    await execa('pnpm', ['install'], {cwd: clone})
+    await execa(path.join(process.cwd(), 'node_modules/.bin/link'), [process.cwd()], {cwd: clone})
+    const {all: lint} = await execa(
+      path.join(process.cwd(), 'node_modules/.bin/eslint'),
+      ['.', '--fix', '--max-warnings', '0'],
+      {
         all: true,
         cwd: clone,
         reject: false,
-      })
+      },
+    )
 
-      const snapshot = lint
-        .replaceAll(new RegExp(`${repoName}@\\S+`, 'g'), `${repoName}@<version>`)
-        .replaceAll(tmp, '<dir>')
-        .replaceAll('/private<dir>/', '<dir>/')
-        .replaceAll(ts, '<timestamp>')
-        .replaceAll(/\d+:\d+/g, '<line>:<col>')
-      try {
-        expect(snapshot).toMatchSnapshot()
-      } catch (error) {
-        console.error(`\n\nFailed to match snapshot for ${repoUrl}\n\n${snapshot}\n\n`)
-      }
-    },
-  )
+    const snapshot = lint
+      .replaceAll(new RegExp(`${repoName}@\\S+`, 'g'), `${repoName}@<version>`)
+      .replaceAll(tmp, '<dir>')
+      .replaceAll('/private<dir>/', '<dir>/')
+      .replaceAll(ts, '<timestamp>')
+      .replaceAll(/\d+:\d+/g, '<line>:<col>')
+    try {
+      expect(snapshot).toMatchSnapshot()
+    } catch {
+      console.error(`\n\nFailed to match snapshot for ${repoUrl}\n\n${snapshot}\n\n`)
+    }
+  })
 }
